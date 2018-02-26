6c7b89d8324318e5f9cfc5969104001fdc70aaab
Philip Wysocki <philwyso@gmail.com>
2018-02-26 11:50:45 -0500

    Update qbRequest.php

--- pw65/beta/qbRequest.php
+++ pw65/beta/qbRequest.php
@@ -23,25 +23,20 @@
   $result = $conn->query($sql);
   
   $fail = fail;
-  $i = -1
+  $i = -1;
   if ($result->num_rows == 0) {
     echo json_encode($fail);
   } elseif ($result->num_rows > 0) {
   	while($row = $result->fetch_assoc()) { 
       $i++;
-      $output = array(
-       '$i' => (
-          'id' => $row["id"];
-          'name' = $row["qName"];
-          'question' = $row["question"];
-          'topic' = $row["topic"];
-          'difficulty' = $row["difficulty"];
-          'cases' = $row["cases"];
-          'input' = $row["input"];
-          'output' = $row["output"];
-          )
-      );
-    
+      $output[$i]['id'] = $row["id"];
+      $output[$i]['qName'] = $row["qName"];
+      $output[$i]['question'] = $row["question"];
+      $output[$i]['topic'] = $row["topic"];
+      $output[$i]['difficulty'] = $row["difficulty"];
+      $output[$i]['cases'] = $row["cases"];
+      $output[$i]['input'] = $row["input"];
+      $output[$i]['output'] = $row["output"];
       
     }
   }
