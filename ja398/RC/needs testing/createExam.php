1052421a440c73d63634f4ee38f1711d2fed44f7
Jay A <jayarroyo@users.noreply.github.com>
2018-04-01 03:03:41 -0400

    Add files via upload

--- ja398/RC/needs testing/createExam.php
+++ ja398/RC/needs testing/createExam.php
@@ -9,30 +9,18 @@ catch (Exception $e) {
 	echo json_encode($message);
 	exit;
 }
-if (isset($_POST['examName'])) {
-	$examName = mysqli_real_escape_string($db, $_POST['examName'];
-	$query 	= "SELECT QuestionList FROM Exams WHERE ExamName = '$examName';";
-	$result = mysqli_query($db, $query);
-	$row = mysqli_fetch_row($result);
-	$id_array = explode(",", $row[0]);
-	foreach ($id_array as $id) {
-		$id_query 	= "SELECT * FROM QuestionBank WHERE ID = '$id';";
-		$id_result = mysqli_query($db, $query);
-		while ($row = mysqli_fetch_assoc($id_result)) {
-			$i 							   = $row['ID'];
-			$row_array[$i]['functionName'] = $row['FunctionName'];
-			$row_array[$i]['topic'] 	   = $row['Topic'];
-			$row_array[$i]['difficulty']   = $row['Difficulty'];
-			$row_array[$i]['question'] 	   = $row['Question'];
-			$row_array[$i]['cases'] 	   = $row['Cases'];
-			$row_array[$i]['input'] 	   = $row['Input'];
-			$row_array[$i]['output'] 	   = $row['Output'];
-			$row_array[$i]['pointWorth']   = $row['PointWorth'];
-		}
-	}
-	echo json_encode($row_array);
-	//mysqli_free_result($result);
+if (isset($_POST['examName'], $_POST['questionList'], $_POST['pointList']) {
+    $examName     = mysqli_real_escape_string($db, $_POST['examName']);
+    $questionList = mysqli_real_escape_string($db, $_POST['questionList']);
+    $pointList    = mysqli_real_escape_string($db, $_POST['pointList']);
+    $examStatus = "assigned";
+    $examInsert = "INSERT INTO ExamList (ExamName, QuestionList, PointList) 
+                   VALUES ('$examName','$questionList','$pointList');";
+    $assignExam = "UPDATE ExamList SET ExamStatus = '$examStatus' WHERE ExamName = '$examName';";
+
+    if(mysqli_query($db, $examInsert) && mysqli_query($db, $assignExam)) { $message = "success"; }
+    else { $message = "fail"; }
+    echo json_encode($message);
 }
 mysqli_close($db);
 ?>
-	
